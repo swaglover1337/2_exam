7163472b8ed29e89ee3ea8561489aaeb59689f22
u31511 <u31511@P31511>
2026-06-02 14:22:41 +1000

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2_exam
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static List<User> User { get; set; } = new List<User>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (User.Count == 0)
+            {
+                User.Add(new User { Login = "admin", Password = "admin", IsAdmin = true });
+                User.Add(new User { Login = "user", Password = "user", IsAdmin = false });
+            }
+
+            MainFrame.Navigate(new AuthPage());
+        }
+    }
+}
